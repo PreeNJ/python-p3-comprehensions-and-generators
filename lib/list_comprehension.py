e398fe1dfd5b2b608dd5ac52f601667e725ff9b6
Priscillah Njai <priscillahnjaii@gmail.com>
2025-05-14 23:06:26 +0300

    Finished Sequences

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+#def return_evens(num_list):
+    #pass
+
+#def make_exclamation(sentence_list):
+    #pass
+#!/usr/bin/env python3
+
 def return_evens(num_list):
-    pass
+    """
+    Given a sequence of integers, return a new list containing only the even ones.
+    """
+    return [n for n in num_list if n % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    """
+    Given a list of strings, return a new list where each string has '!' appended.
+    """
+    return [s + "!" for s in sentence_list]
